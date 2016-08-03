d1f3333095086d798d14d61cca069e89bf9fb8ba
tira <YWachyar@nl.ibm.com>
2016-08-03 15:25:07 +0200

    modifying basemap

--- web-gis-app/js/maps/init-map.js
+++ web-gis-app/js/maps/init-map.js
@@ -5,7 +5,6 @@ var jakartaView = [-6.178520, 106.827207];
 
 views.push(indonesiaView, jakartaView);
 
-
 var mymap = L.map('mapid').setView(views[1], 10);
 var osm = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var mapbox = 'https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGlyYTI4IiwiYSI6ImNpcmM0bWhhZTAwNnNpa25ub3hkbWI2bnAifQ.m6nEUKku6DmUSTljCgNJ4g';
@@ -15,26 +14,31 @@ var sources = [osm,mapbox];
 L.tileLayer(sources[1]).addTo(mymap);
 
 // creating marker on the map
-
-var jakartaPoint = [-6.178520, 106.827207];
-var depokPoint = [-6.367967, 106.829559];
-var markerPoints = [];
-markerPoints.push(jakartaPoint, depokPoint);
-
-var cities = ['Jakarta', 'Depok'];
-
-var citiesStats = [
+var cities = [
   {
-    name:'Jakarta',
-    population: 9680000
+    name: 'Jakarta',
+    population: 9680000,
+    coordinates: [-6.178520, 106.827207]
   },
   {
     name: 'Depok',
-    population: 1752000
+    population: 1752000,
+    coordinates: [-6.367967, 106.829559]
   }
-];
+]
 
-for (var i = 0; i<markerPoints.length; ++i) {
-  var marker = L.marker(markerPoints[i], {draggable:false}).addTo(mymap);
-  marker.bindPopup('Welcome to ' + citiesStats[i].name + '<br>' + 'Population: ' + citiesStats[i].population);
+for (var i = 0; i<cities.length; ++i) {
+  var marker = L.marker(cities[i].coordinates, {draggable:false}).addTo(mymap);
+  marker.bindPopup('Welcome to ' + cities[i].name + '<br>' + 'Population: ' + cities[i].population);
 }
+
+// creating clickable popup
+var popup = L.popup();
+function onMapClick(e){
+  popup
+  .setLatLng(e.latlng)
+  .setContent('Coordinates: <br>' + e.latlng.toString())
+  .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
